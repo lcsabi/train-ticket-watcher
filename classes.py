e17ea39937f7c552924977a38f75e252b334f972
László Csaba <laszlo.cs4ba@gmail.com>
2024-02-25 20:05:49 +0100

    refactor: implement classes and parser

--- classes.py
+++ classes.py
@@ -14,7 +14,7 @@ class Ticket:
         return ("Full price: " + str(self.full_ticket_price_eur) + " EUR / "
                 + str(self.full_ticket_price_huf) + " HUF"
                 + (("\nDiscounted price: " + str(self.discounted_ticket_price_eur)) if (self.discounted_ticket_price_eur < self.full_ticket_price_eur) else "")
-                + "\nStops: " + " - ".join(self.stops)
+                + "\n" + str(len(self.stops)) + " Stops: " + " - ".join(self.stops)
                 + "\nOffer valid until: " + self.valid_until)
 
 
@@ -38,6 +38,7 @@ class Offer:
         self.routes.append(route)
 
     def __repr__(self):
+        # TODO: Add start and end stations to repr, create station code mapping
         string_repr = "*" * 40 + "\n"
         for ticket in self.tickets:
             string_repr += str(ticket)
--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 import json
+from typing import List
 
 import requests
+from requests import Response
 
 import config
 from classes import Ticket, Offer
@@ -8,27 +10,36 @@ from classes import Ticket, Offer
 url = config.endpoint
 headers = config.headers
 data = config.payload
-response = requests.post(url, headers=headers, json=data)
+response: Response = requests.post(url, headers=headers, json=data)
 
 if response.status_code == 200:
     try:
-        parsed_response = response.json()
-        offers_response = parsed_response["route"]
-        all_offers = []
+        parsed_response: dict = response.json()
+        offers_response: dict = parsed_response["route"]
+        all_offers: List[Offer] = []
 
         for offer in offers_response:
-            offer_object = Offer(offer)
-            tickets_response = offer["details"]["tickets"]
+            if offer["transfersCount"] > 1:
+                continue
+            offer_object: Offer = Offer(offer)
+            tickets_response: dict = offer["details"]["tickets"]
             for ticket in tickets_response:
-                ticket_object = Ticket(ticket)
+                ticket_object: Ticket = Ticket(ticket)
                 offer_object.add_ticket(ticket_object)
 
             all_offers.append(offer_object)
-            print(offer_object)
 
-            routes = offer["details"]["routes"]
+            routes: dict = offer["details"]["routes"]
+
+        # TODO: Implement sorting logic for offers based on ticket prices
+        # sorted_offers = sorted(all_offers, key=lambda x: min(lambda y: x.tickets))
+        # print(sorted_offers)
+        print(all_offers)
     except json.decoder.JSONDecodeError:
         print("Failed to parse JSON response.")
 else:
     print("Request unsuccessful:", response.status_code)
     print(response.text)
+
+# Get tickets for 16 days in advance
+# Top 2 tickets / day
