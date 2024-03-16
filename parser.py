df427f40735f8838528003e1e78b289c2c6c309d
lcsabi <laszlo.cs4ba@gmail.com>
2024-03-16 21:40:23 +0100

    refactor: add information about transfers, edit last station information

--- parser.py
+++ parser.py
@@ -12,7 +12,7 @@ class OfferParser:
     def parse_offer(raw_offer_dict) -> dict:
         current_offer_dict = {
             'details': OfferParser.parse_offer_details(raw_offer_dict),
-            'tickets': OfferParser.parse_offer_tickets(raw_offer_dict['details']['tickets']),
+            'tickets': OfferParser.parse_offer_tickets(raw_offer_dict['details']['tickets'], raw_offer_dict['lastStation']),
             'routes': OfferParser.parse_offer_routes(raw_offer_dict['details']['routes'])
         }
         return current_offer_dict
@@ -23,6 +23,7 @@ class OfferParser:
             'from': raw_details_dict['details']['routes'][0]['startStation']['name'],
             'to': raw_details_dict['lastStation'],
             'transfers': raw_details_dict['transfersCount'],
+            'transfer_stations': [route['destionationStation']['name'] for route in raw_details_dict['details']['routes'][:-1]],
             'departure_time': ' '.join(raw_details_dict['departure']['time'].split('+')[0].split('T')),
             'arrival_time': ' '.join(raw_details_dict['arrival']['time'].split('+')[0].split('T')),
             'total_travel_time': raw_details_dict['travelTimeMin']
@@ -30,7 +31,7 @@ class OfferParser:
         return parsed_details
 
     @staticmethod
-    def parse_offer_tickets(raw_tickets_list: List[dict]) -> List[dict]:
+    def parse_offer_tickets(raw_tickets_list: List[dict], last_station: str) -> List[dict]:
         parsed_tickets = []
         for ticket in raw_tickets_list:
             current_ticket_dict = {
@@ -39,7 +40,7 @@ class OfferParser:
                 'discounted_price_huf': ticket['discountedGrossPrice']['amountInDefaultCurrency'],
                 'discounted_price_eur': ticket['discountedGrossPrice']['amount'],
                 'number_of_stops': len(ticket['touchedStations']),
-                'names_of_stops': [station['name'] for station in ticket['touchedStations'] if station['name']] + [ticket['endStation']['name']],
+                'names_of_stops': [station['name'] for station in ticket['touchedStations'] if station['name']] + [last_station],
                 'offer_valid_until': ' '.join(ticket['offerValidTo'].split('+')[0].split('T'))
             }
             parsed_tickets.append(current_ticket_dict)
