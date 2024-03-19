af69226ffe03da34121a47ff459fab9d18bc3c47
lcsabi <laszlo.cs4ba@gmail.com>
2024-03-19 10:49:37 +0100

    feat: add console handler to logger config for future use

--- logger_config.py
+++ logger_config.py
@@ -2,18 +2,24 @@ import logging
 
 
 def setup_logger(name: str):
-    # Configure the root logger
+    # Set logger level
     logging.basicConfig(level=logging.DEBUG)
 
     # Create a file handler
     file_handler = logging.FileHandler('app.log')
 
+    # Create a console handler
+    # console_handler = logging.StreamHandler()
+    # console_handler.setLevel(logging.DEBUG)
+
     # Create a formatter and set it for the handlers
     formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
     file_handler.setFormatter(formatter)
+    # console_handler.setFormatter(formatter)
 
-    # Get the root logger
+    # Create logger and add handlers
     logger = logging.Logger(name)
     logger.addHandler(file_handler)
+    # logger.addHandler(console_handler)
 
     return logger
